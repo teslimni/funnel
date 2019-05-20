a1de0dca988bcd3c6e085c0bb7ecab46d1cf3e16
Teslim Adeyemo <teslimni@gmail.com>
2019-05-20 03:58:03 +0100

    Initial project commit

---
+++ main.js
@@ -0,0 +1,1 @@
+(function ($) { window.fnames = new Array(); window.ftypes = new Array(); fnames[1] = 'FNAME'; ftypes[1] = 'text'; fnames[0] = 'EMAIL'; ftypes[0] = 'email'; fnames[3] = 'ADDRESS'; ftypes[3] = 'address'; fnames[4] = 'PHONE'; ftypes[4] = 'phone'; }(jQuery)); var $mcj = jQuery.noConflict(true);
